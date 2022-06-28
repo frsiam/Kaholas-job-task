307ff4507b377c9a6b822569b8f156ce605c6ef2
frsiam <siam.frs@gmail.com>
2022-06-28 08:59:59 +0600

    inititalize the project and add json data then load the data into my app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [mydata, setMydata] = useState();
+  useEffect(() => {
+    fetch('http://localhost:3000/data.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data[0].campaignName)
+        return setMydata(data[0])
+      })
+  }, [])
+  console.log(mydata?.paintingImages)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='max-w-6xl mx-auto min-h-screen py-8'>
+      <header>
+        <h1 className='text-orange-600'>Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gogh, Matisse, & More</h1>
+        <p>Multi - Day Course ?</p>
       </header>
     </div>
   );
